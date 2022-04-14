1735b2afe11cdafb8bd0ca57b090c6f6c9466d7c
Davidson Goes <davidsongoes@gmail.com>
2022-04-14 15:08:20 -0300

    test(unique-service): :sparkles: Implementa mais teste

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,23 @@
+import { UniqueIdService } from "./unique-id.service";
+// describe("O artefato que queremos testar", () => {
+//   it("Primeira condição que queremos testar", () => {});
+//   it("Segunda condição que queremos testar", () => {});
+// });
+
+// blablabla should blablabla when blablabla
+
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+    const service = new UniqueIdService();
+    const id = service.generateUniqueIdWithPrefix("app");
+    expect(id.startsWith("app-")).toBeTrue();
+  });
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+    for (let i = 0; i < 20; i++) {
+      ids.add(service.generateUniqueIdWithPrefix("app"));
+    }
+    expect(ids.size).toBe(20);
+  });
+});
