a6c886d693e1a5fcc670ebc7688040799b69ca18
Davidson Góes <davidsongoes@gmail.com>
2022-04-17 14:47:23 -0300

    feat(like-widget): :sparkles: Implementa fixture no arquivo de teste

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,13 +1,21 @@
-import { TestBed } from "@angular/core/testing";
-import { UniqueIdService } from "../../services/unique-id/unique-id.service";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { UniqueIdService } from "./../../services/unique-id/unique-id.service";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { LikeWidgetComponent } from "./like-widget.component";
 
 describe(LikeWidgetComponent.name, () => {
+  let fixture: ComponentFixture<LikeWidgetComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LikeWidgetComponent],
+      imports: [FontAwesomeModule],
+      providers: [UniqueIdService],
     }).compileComponents();
+    fixture = TestBed.createComponent(LikeWidgetComponent);
   });
-
-  it(``, () => {});
+  it(`Should create component`, () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
+  });
 });
