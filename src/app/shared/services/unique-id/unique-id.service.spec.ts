0a608aab6757773cb3567093a9defb3f5f160df1
Davidson Góes <davidsongoes@gmail.com>
2022-04-17 09:39:13 -0300

    test(unique-service): :wrench: Isolando instância do service para cada teste

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -7,17 +7,25 @@ import { UniqueIdService } from "./unique-id.service";
 // blablabla should blablabla when blablabla
 
 describe(UniqueIdService.name, () => {
+  let service: UniqueIdService = null;
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix("app");
     expect(id.startsWith("app-")).toBeTrue();
   });
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-    const service = new UniqueIdService();
     const ids = new Set();
     for (let i = 0; i < 20; i++) {
       ids.add(service.generateUniqueIdWithPrefix("app"));
     }
     expect(ids.size).toBe(20);
+  });
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generatedIds when called`, () => {
+    for (let i = 0; i < 5; i++) {
+      service.generateUniqueIdWithPrefix("app-");
+    }
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(5);
   });
 });
